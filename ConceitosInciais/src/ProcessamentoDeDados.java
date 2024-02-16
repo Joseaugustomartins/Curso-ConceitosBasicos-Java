4413f71c1ace4d5937c6c344536e170145524b0f
JOSE AUGUSTO MARTINS QUEIROZ <joseaugustomartinsqueiroz@gmail.com>
2024-02-16 14:32:25 -0300

    Create ProcessamentoDeDados.java

---
+++ ConceitosInciais/src/ProcessamentoDeDados.java
@@ -0,0 +1,60 @@
+
+public class ProcessamentoDeDados {
+
+	public static void main(String[] args) {
+		
+		int  x,y;
+		
+		x = 5;
+		 
+		y = 2 * x;
+		
+		System.out.println(x);
+		System.out.println(y);
+		
+		System.out.println("---------------------------");
+		
+		int x1;
+		double y1;
+		
+		x1 = 5;
+		y1 = 2 * x;
+		
+		System.out.println(x1);
+		System.out.println(y1);
+		
+		System.out.println("---------------------------");
+		
+		double b = 6.0;
+		double B = 8.0;
+		double h = 5.0;
+		
+		double area = (b + B) / 2.0 * h;
+		
+		System.out.println(area);
+		
+		System.out.println("---------------------------");
+		
+		int a, b1;
+		double resultado;
+		
+		a = 5;
+		b1 = 2;
+		
+		resultado = (double) a / b1;
+		
+		System.out.println(resultado);
+		
+		System.out.println("---------------------------");
+		
+		double a1;
+		int b2;
+		
+		a1 = 5.0;
+		b2 = (int) a1;
+		
+		System.out.println(b2
+				);
+	}
+
+}
