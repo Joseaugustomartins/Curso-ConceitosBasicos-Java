db6d4e02d500eec57ac2a35d21a6784dec4cfb46
JOSE AUGUSTO MARTINS QUEIROZ <joseaugustomartinsqueiroz@gmail.com>
2024-03-19 14:51:00 -0300

    Create Ex1115.java

---
+++ ConceitosInciais/src/UFI/Ex1115.java
@@ -0,0 +1,30 @@
+package URF;
+
+import java.util.Scanner;
+
+public class Ex1115 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		
+		while(x != 0 && y != 0) {
+			if(x > 0 && y > 0) {
+				System.out.println("Primeiro");
+			}else if(x < 0 && y > 00) {
+				System.out.println("Segundo");
+			}else if(x < 0 && y < 0) {
+				System.out.println("Terceiro");
+			}else {
+				System.out.println("Quarto");
+			}
+			x = sc.nextInt();
+			y = sc.nextInt();
+		}
+		
+		sc.close();
+	}
+
+}
