3ee571e7be3d829ff09bac64af49b20d5d2f478c
JOSE AUGUSTO MARTINS QUEIROZ <joseaugustomartinsqueiroz@gmail.com>
2024-03-28 14:20:44 -0300

    Update AtribuicaoAcumulativa.java

--- ConceitosInciais/src/AtribuicaoAcumulativa.java
+++ ConceitosInciais/src/AtribuicaoAcumulativa.java
@@ -1,10 +1,8 @@
-import java.util.Locale;
 import java.util.Scanner;
 
 public class AtribuicaoAcumulativa {
 
 	public static void main(String[] args) {
-		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
 		int minutos = sc.nextInt();
